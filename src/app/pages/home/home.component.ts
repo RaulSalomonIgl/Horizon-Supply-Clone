4da19739253ab68a73ffafe645871ff4d170b9ec
RaulSalomonIgl <iglesiasraularturo2001@outlook.com>
2025-03-07 11:41:17 -0600

    feat: :sparkles: implement the Pagination component to limit the products displayed
    The Pagination component is implemented on the home page to control and limit the number of products displayed, improving navigation and data loading.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,22 +4,29 @@ import { PRODUCT_REPOSITORY_PROVIDER } from '../../modules/product/data/reposito
 import { GetProductsUseCase } from '../../modules/product/core/use-cases/get-products.use-case';
 import { Observable } from 'rxjs';
 import { Product } from '../../modules/product/core/entities/product.model';
-import { CommonModule, CurrencyPipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { PaginationComponent } from '../../components/pagination/pagination.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ProductCardComponent, CurrencyPipe],
+  imports: [CommonModule, ProductCardComponent, PaginationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   providers: [PRODUCT_REPOSITORY_PROVIDER, GetProductsUseCase],
 })
 export class HomeComponent implements OnInit {
   products$: Observable<Product[]> = new Observable();
+  pagedProducts: Product[] = []; // Productos de la página actual
 
   constructor(private getProducts: GetProductsUseCase) {}
 
   ngOnInit(): void {
     this.products$ = this.getProducts.execute();
+  }
+
+  // Actualizar la lista de productos cuando cambie la página
+  onPageChange(pagedItems: Product[]): void {
+    this.pagedProducts = pagedItems;
   }
 }
